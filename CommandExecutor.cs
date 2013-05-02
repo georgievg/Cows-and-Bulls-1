e22098e042d81f892aa0ee40748dc0e89f6b8f07
Vasil <telerik.vasko@gmail.com>
2013-05-02 23:25:54 +0300

    Added CommandExecutor for later development.

---
+++ CommandExecutor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BullsAndCows
+{
+    // This class should have some command parser logic
+    public class CommandExecutor
+    {
+        // TO DO:
+        
+    }
+}
